778e1fa7da5b9d2c59d1e20a1a55c7e984e655de
nidhiupman568 <upmanyunidhi3092@gmail.com>
2024-07-15 21:58:55 +0530

    make dice roll app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js File 
+import React from 'react'; 
+import RollDice from './Components/RollDice'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas) 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() { 
+	return ( 
+		<div> 
+			<RollDice /> 
+		</div> 
+	); 
+} 
 
-export default App;
+export default App; 
