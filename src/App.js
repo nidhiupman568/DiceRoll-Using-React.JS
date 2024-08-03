4533c7242a7aeacbf30f512ada48f433e1aa34ec
nidhiupman568 <upmanyunidhi3092@gmail.com>
2024-08-03 23:09:28 +0530

    do some creative things

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-// App.js File 
-import React from 'react'; 
-import RollDice from './Components/RollDice'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-library.add(fas) 
+// App.js
+import React from 'react';
+import RollDice from './Components/RollDice';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
-function App() { 
-	return ( 
-		<div> 
-			<RollDice /> 
-		</div> 
-	); 
-} 
+library.add(fas);
 
-export default App; 
+function App() {
+  // Inline styles for centering
+  const centerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh', // Full viewport height
+    margin: 0
+  };
+
+  return (
+    <div style={centerStyle}>&nbsp&nbsp&nbsp&nbsp&nbsp
+      <h1>Dice Roll App using ReactJS - By Nidhi Upman</h1>
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
